4bccd4b7635174b11a3d46d8e4d6fb152fd31f61
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-04-30 17:42:30 +0500

    Load more button postioned to center

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { MdOutlineStar } from "react-icons/md";
-import { MdDiscount } from "react-icons/md";
+import { MdOutlineStar, MdDiscount } from "react-icons/md";
 
 const truncateDescription = (description, maxLength) =>
   description.length > maxLength
@@ -21,23 +20,25 @@ const renderStockStatus = (stock) => {
 
 const ProductCardSkeleton = () => (
   <div className="flex bg-slate-800 justify-between w-[300px] h-[430px] flex-col px-3 rounded-xl pb-3 shadow-lg shadow-slate-400 text-white py-[10px]">
-    <div className="skeleton w-[278px] h-[185px] rounded-lg"></div>
-    <div className="skeleton h-[19px] w-[80%] mt-2"></div>
-    <div className="skeleton h-[15px] w-[90%] mt-2"></div>
-    <div className="flex items-center gap-1 text-[15px] mt-2">
-      <div className="skeleton h-5 w-5"></div>
-      <div className="skeleton h-5 w-[20%]"></div>
-    </div>
-    <div className="flex justify-between mt-2">
-      <div className="skeleton h-[15px] w-[50%]"></div>
-      <div className="skeleton h-[15px] w-[20%]"></div>
-    </div>
-    <div className="flex gap-1 items-center mt-2">
-      <div className="skeleton h-5 w-5"></div>
-      <div className="skeleton h-5 w-[60%]"></div>
-    </div>
-    <div className="flex items-center justify-center mt-2">
-      <div className="skeleton h-10 w-[60%]"></div>
+    <div className="flex bg-slate-800 justify-between w-[300px] h-[430px] flex-col px-3 rounded-xl pb-3 shadow-lg shadow-slate-400 text-white py-[10px]">
+      <div className="skeleton w-[278px] h-[185px] rounded-lg"></div>
+      <div className="skeleton h-[19px] w-[80%] mt-2"></div>
+      <div className="skeleton h-[15px] w-[90%] mt-2"></div>
+      <div className="flex items-center gap-1 text-[15px] mt-2">
+        <div className="skeleton h-5 w-5"></div>
+        <div className="skeleton h-5 w-[20%]"></div>
+      </div>
+      <div className="flex justify-between mt-2">
+        <div className="skeleton h-[15px] w-[50%]"></div>
+        <div className="skeleton h-[15px] w-[20%]"></div>
+      </div>
+      <div className="flex gap-1 items-center mt-2">
+        <div className="skeleton h-5 w-5"></div>
+        <div className="skeleton h-5 w-[60%]"></div>
+      </div>
+      <div className="flex items-center justify-center mt-2">
+        <div className="skeleton h-10 w-[60%]"></div>
+      </div>
     </div>
   </div>
 );
@@ -94,9 +95,7 @@ const Cards = () => {
                     <MdDiscount />
                     {product.discountPercentage}%
                   </p>
-                  <p className="flex">
-                    {renderStockStatus(product.stock)} {}
-                  </p>
+                  <p className="flex">{renderStockStatus(product.stock)}</p>
                 </div>
                 <h2 className="flex gap-1 items-center">
                   Price:
